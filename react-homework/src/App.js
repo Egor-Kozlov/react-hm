2598dd5c2cb5f3626df70b0b0436f66496daa1e8
Egor-Kozlov <egor.s.kozlov@gmail.com>
2021-03-04 15:19:02 +0300

    commit

--- react-homework/src/App.js
+++ react-homework/src/App.js
@@ -9,11 +9,16 @@ import Square from "./components/hm-1/Ex1";
 import './components/hm-1/Ex2';
 import CheckboxFontStyle from "./components/hm-1/Ex2";
 
+// import Ex3 components
+import './components/hm-2/Ex3';
+import ButtonSignIn from "./components/hm-2/Ex3";
+
 
 function App() {
   return (
     <div className="wrap">
-      <div className="home-work-1">
+      {/* START HM-1 */}
+      <div className="home-work-title-container">
         <p>#1 Home work</p>
       </div>
       <div className="squares-container">
@@ -30,12 +35,21 @@ function App() {
           cssClass="small-square" 
         />
       </div>
-      <div className="home-work-1">
+      {/* END HM-1 */}
+      {/* START HM-2 */}
+      <div className="home-work-title-container">
         <p>#2 Home work</p>
       </div>
       <div className='text-container'>
         <CheckboxFontStyle />
       </div>
+      {/* END HM-2 */}
+      {/* START HM-3 */}
+      <div className="home-work-title-container">
+        <p>#3 Home work</p>
+      </div>
+      {/* END HM-3 */}
+      <ButtonSignIn />
     </div>
   );
 }
